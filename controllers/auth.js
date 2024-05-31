a0bdb6bbb36fab14ac60507745dd0f51bf3ba370
Gabriel <juanmendez0013@gmail.com>
2024-05-31 18:32:16 -0300

    se genero el jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require("express");
 const bcrypt = require("bcryptjs");
 const Usuario = require("../models/Usuario");
+const { generarJWT } = require("../helpers/jwt");
 
 const crearUsuario = async (req, res = response) => {
   const { email, password } = req.body;
@@ -23,10 +24,14 @@ const crearUsuario = async (req, res = response) => {
 
     await usuario.save();
 
+    //Generar JWT
+    const token = await generarJWT(usuario.id, usuario.name);
+
     res.status(201).json({
       ok: true,
       uid: usuario.id,
       name: usuario.name,
+      token,
     });
   } catch (error) {
     res.status(500).json({
@@ -61,11 +66,13 @@ const loginUsuario = async (req, res = response) => {
     }
 
     // Generar nuestro JWT
+    const token = await generarJWT(usuario.id, usuario.name);
 
     res.json({
       ok: true,
       uid: usuario.id,
       name: usuario.name,
+      token,
     });
   } catch (error) {
     res.status(500).json({
