bb521a449a9b683efa7c17c01097f78221c564e7
Gabriel <juanmendez0013@gmail.com>
2024-05-31 19:36:53 -0300

    se agrego cors

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const { dbConnection } = require("./database/config");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
 //Base de datos
 dbConnection();
+
+// CORS
+app.use(cors());
 
 // Directorio publico
 app.use(express.static("public"));
