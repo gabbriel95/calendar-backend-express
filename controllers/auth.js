66e60f0fe98a1889d1c3b62fa6173d6621acae63
Gabriel <juanmendez0013@gmail.com>
2024-05-31 15:01:43 -0300

    validacion de usuario existente

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,34 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
+const Usuario = require("../models/Usuario");
 
-const crearUsuario = (req, res = response) => {
-  const { name, email, password } = req.body;
+const crearUsuario = async (req, res = response) => {
+  const { email, password } = req.body;
 
-  res.status(201).json({
-    ok: true,
-    msg: "registro",
-    name,
-    email,
-    password,
-  });
+  try {
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El usuario ya existe",
+      });
+    }
+
+    usuario = new Usuario(req.body);
+
+    await usuario.save();
+
+    res.status(201).json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
 };
 
 const loginUsuario = (req, res = response) => {
