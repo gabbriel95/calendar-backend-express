f83d6fbd98504603d48b0638aed29e6bb7d05424
Gabriel <juanmendez0013@gmail.com>
2024-05-31 16:44:28 -0300

    login de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,15 +36,43 @@ const crearUsuario = async (req, res = response) => {
   }
 };
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async (req, res = response) => {
   const { email, password } = req.body;
 
-  res.status(200).json({
-    ok: true,
-    msg: "login",
-    email,
-    password,
-  });
+  try {
+    const usuario = await Usuario.findOne({ email });
+
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El usuario no existe",
+      });
+    }
+
+    //Confirmar passwords
+
+    const validPassword = bcrypt.compareSync(password, usuario.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Password incorrecto",
+      });
+    }
+
+    // Generar nuestro JWT
+
+    res.json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
 };
 
 const revalidadToken = (req, res = response) => {
