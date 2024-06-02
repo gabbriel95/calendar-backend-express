7bd77c5d3c410d269544f0b706602a43084e3183
Gabriel <juanmendez0013@gmail.com>
2024-06-02 19:34:00 -0300

    delete update get events

--- routes/events.js
+++ routes/events.js
@@ -37,7 +37,16 @@ router.post(
 );
 
 //Actualizar evento
-router.put("/:id", [], actualizarEvento);
+router.put(
+  "/:id",
+  [
+    check("title", "El titulo es obligatorio").not().isEmpty(),
+    check("start", "Fecha de inicio es obligatoria").custom(isDate),
+    check("end", "Fecha de finalización es obligatoria").custom(isDate),
+    validarCampos,
+  ],
+  actualizarEvento
+);
 
 router.delete("/:id", [], eliminarEvento);
 
